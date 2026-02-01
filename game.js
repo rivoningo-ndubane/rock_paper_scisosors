29128f01f6a5d9fb510288b345902de327ef7a7e
rivoningo-ndubane <rivinine@gmail.com>
2026-02-01 09:46:25 +0200

    Complete main logic for the ui

--- game.js
+++ game.js
@@ -20,76 +20,78 @@ function getComputerChoice(){
 }
 
 
-// DEPRECATED
-function getHumanChoice(){
-    let instructions = `Please select a valid choice (1-3):
-    
-    1. Rock
-    2. Paper
-    3. Scissors
-    `;
-    let choice = +(prompt(instructions));
-
-    switch(choice){
-        case 1:
-            return "rock";
-        case 2:
-            return "paper";
-        case 3:
-            return "scissors";
-    }
-}
-
-
 function playRound(humanChoice, computerChoice){
+    // Create object to obtain winning matches
     const winningMatch = {
         "rock": "scissors",
         "paper": "rock",
         "scissors": "paper",
     }
 
-    if (humanChoice === computerChoice){
-        console.log("Draw");
-    }
-
-    else if (winningMatch[humanChoice] === computerChoice){
-        console.log("You win! Rock beats Scissors");
-    }
-
-    else {
-        console.log("You lose! Paper beats Rock");
-    }
-}
-
-
-function playGame(){
-    // Main Variables
-    let humanScore = 0;
-    let computerScore = 0;
-
-    for(let i=0; i<5; i++){
-        let result = playRound(getHumanChoice(), getComputerChoice());
-        
-        if (result === 1){humanScore++;}
-        else if (result === 0){computerScore++;}
-    }
-
-    console.log("Game End");
-
-    if (humanScore === computerScore){console.log("The game was a Draw");}
-    else if (humanScore > computerScore){console.log("You won the game!!");}
-    else {console.log("You lost the game!!");}
+    if (humanChoice === computerChoice){}
+    else if (winningMatch[humanChoice] === computerChoice){return 1;}
+    else {return -1;}
 }
 
 
 // Run script
+// Keep track of scores
+let humanScore = 0;
+let computerScore = 0;
+
+// get a nodeList of buttons
 const buttons = document.querySelectorAll("button");
 
+// add event listeners for the buttons
 buttons.forEach(button => {
     button.addEventListener('click', (e) => {
         e.preventDefault();
 
+        // Acquire choices
         const humanChoice = button.id;
-        playRound(humanChoice, getComputerChoice());
+        const computerChoice = getComputerChoice();
+
+        // Play a round
+        const result = playRound(humanChoice, computerChoice);
+
+        // Acquire node that will display results
+        const results = document.querySelector("#results");
+        let display = '';
+
+        // Update scores
+        if (result === 1){
+            humanScore++;
+            display = `\nYou win! ${humanChoice} beats ${computerChoice}`;
+        }
+        else if (result === -1){
+            computerScore++;
+            display = `\nYou lose! ${humanChoice} losses to ${computerChoice}`;
+        }
+        else {
+            display = "\nDraw";
+        }
+
+        // Change display contents to include scores
+        display += `
+        \nHuman Score: ${humanScore}
+        Computer Score: ${computerScore}
+        `
+
+        // End game logic
+        if (humanScore === 5 || computerScore === 5){
+            if (humanScore === computerScore){display += "\nThe game was a Draw";}
+            else if (humanScore > computerScore){display += "\nYou won the game!!";}
+            else {display += "\nYou lost the game!!";}
+
+        }
+
+        // Display to screen
+        results.innerText = display;
+
+        // reset
+        if (humanScore === 5 || computerScore === 5){
+            humanScore=0;
+            computerScore=0;
+        }
     })
 });
